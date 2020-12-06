6de0af6d4f6fee08b3c6cf78a77392ff2a535527
Karol Dzialowski <karlososhd@gmail.com>
2020-12-06 15:46:28 +0100

    :sparkles: MA, IDW interpolation with KDTree

--- lab_01/interpolation.py
+++ lab_01/interpolation.py
@@ -1,9 +1,19 @@
+"""
+TODO:
+    - [ ] get parameters from user
+    - [ ] changing circle to rectangle
+    - [ ] saving to ASCII XYZ
+    - [ ] load in qgis
+"""
+
 import pandas as pd
 import numpy as np
 import tqdm
 import matplotlib
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from scipy.spatial import cKDTree
+import time
 
 
 def load_data():
@@ -14,6 +24,7 @@ def load_data():
 
 
 def grid(df, spacing):
+    # Wyznaczenie punktów skrajnych
     min_x = np.min(df[:, 0])
     max_x = np.max(df[:, 0])
     min_y = np.min(df[:, 1])
@@ -24,63 +35,88 @@ def grid(df, spacing):
     print(f"Y: od {min_y} do {max_y}, odległość: {max_y - min_y}m")
     print("")
 
-    # Tworzenie siatki
+    # Stworzenie siatki
     x = np.arange(min_x, max_x, spacing)
     y = np.arange(min_y, max_y, spacing)
     xx, yy = np.meshgrid(x, y)
     print("Rozmiar siatki grid:", len(x), len(y))
 
-    return xx, yy
+    return xx, yy, x, y
 
 
-def moving_average(data, xx, yy, min_n_points, window_size):
+def structure(data, x, y, window_size):
+    print("Tworzenie struktury przechowującej punkty")
+    t1 = time.time()
+    tree = cKDTree(data[:, 0:2])
+    print("Czas na stworzenie struktury:", time.time() - t1)
+
+    return tree
+
+
+def moving_average(data, tree, xx, yy, min_n_points, window_size):
     zz = np.full_like(xx, np.nan)
 
-    for i in tqdm.tqdm(range(xx.shape[0]), desc=" interpolation"):
+    for i in tqdm.tqdm(range(xx.shape[0]), desc="Interpolacja MA"):
         for j in range(xx.shape[1]):
-            # TODO: wylicz wartosc Z - interpolacja ;)
-            # Należy znaleźć takie punkty które są w zasięgu xx[i, j], yy[i, j]
-            # Odległe maksymalnie o window_size
-            # Przygotowac macierz z odległościami?
-            #   Dla każdej możliwej kombinacji i, j obliczyć odległości wszystkich punktów
-            # Uzyc cdist https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html
-            # Moze przed petla?
-            zz[i, j] = 0
+            nearby_points = tree.query_ball_point([xx[i, j], yy[i, j]], window_size)
+            if len(nearby_points) > min_n_points:
+                zz[i, j] = np.mean(data[nearby_points, 2])
 
     return zz
 
 
-def plot_surface(X, Y, Z):
+def idw(data, tree, xx, yy, min_n_points, window_size):
+    zz = np.full_like(xx, np.nan)
+    from scipy.spatial.distance import cdist
+
+    for i in tqdm.tqdm(range(xx.shape[0]), desc="Interpolacja IDW"):
+        for j in range(xx.shape[1]):
+            current_point = [xx[i, j], yy[i, j]]
+            nearby_points = tree.query_ball_point(current_point, window_size)
+            distances = cdist([current_point], data[nearby_points][:, 0:2])
+
+            if len(nearby_points) > min_n_points:
+                denominator = distances[0] ** 2 + 1e-10
+                zz[i, j] = (np.sum(data[nearby_points, 2] / denominator)) / (
+                    np.sum(np.ones_like(denominator) / denominator)
+                )
+
+    return zz
+
+
+def plot(xx, yy, zz):
     fig = plt.figure()
-    ax = fig.gca(projection="3d")
 
-    X, Y = np.meshgrid(X, Y)
+    ax1 = fig.add_subplot(121, projection="3d")
+    ax1.plot_surface(xx, yy, zz, color="b")
 
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    ax2 = fig.add_subplot(122)
+    depth_map = ax2.imshow(zz, interpolation="none")
+    plt.colorbar(depth_map)
 
-    fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
 
 
 def main():
-    df = load_data()
-    print(df.head())
-    print("")
-    data = df.to_numpy()
+    spacing = 0.2
+    min_n_points = 1
+    window_size = 0.4
+    file_path = "./data/wraki_utm.txt"
 
-    # Spytanie użytkownika o parametry
-    spacing = float(input("Rozdzielczość: "))
-    min_n_points = int(input("Minimalna liczba punktów na siatkę: "))
-    window_size = float(input("Rozmiar okna interpolacji: "))
+    t1 = time.time()
+    print("Wczytywanie danych...")
+    data = np.loadtxt(file_path)
+    print("Wczytano dane w:", time.time() - t1, "sekund")
 
-    # spacing = 0.5
-    # min_n_points = 1
-    # window_size = 1
+    xx, yy, x, y = grid(data, spacing)
 
-    xx, yy = grid(data, spacing)
-    print(xx.shape)
+    tree = structure(data, x, y, window_size)
 
-    zz = moving_average(data, xx, yy, min_n_points, window_size)
+    zz = moving_average(data, tree, xx, yy, min_n_points, window_size)
+    plot(xx, yy, zz)
+
+    zz = idw(data, tree, xx, yy, min_n_points, window_size)
+    plot(xx, yy, zz)
 
 
 if __name__ == "__main__":
